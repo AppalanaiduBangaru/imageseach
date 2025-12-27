50d3b3096c8060aba28b0358406d21aee9e1656a
AppalanaiduBangaru <146061359+AppalanaiduBangaru@users.noreply.github.com>
2025-12-27 11:05:59 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,35 +1,40 @@
 const accessKey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
 
-const formEl = document.querySelector("form");
+const formEl = document.getElementById("form");
 const searchInputEl = document.getElementById("search-input");
-const searchResultsEl = document.querySelector(".search-results");
+const searchResultsEl = document.getElementById("search-results");
 const showMoreButtonEl = document.getElementById("show-more-button");
 
 let inputData = "";
 let page = 1;
+const imagesPerPage = 20; 
 
 async function searchImages() {
   inputData = searchInputEl.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&per_page=${imagesPerPage}&client_id=${accessKey}`;
 
   const response = await fetch(url);
   const data = await response.json();
+
   if (page === 1) {
     searchResultsEl.innerHTML = "";
   }
 
   const results = data.results;
 
-  results.map((result) => {
+  results.forEach((result) => {
     const imageWrapper = document.createElement("div");
     imageWrapper.classList.add("search-result");
+
     const image = document.createElement("img");
     image.src = result.urls.small;
-    image.alt = result.alt_description;
+    image.alt = result.alt_description || "Image";
+
     const imageLink = document.createElement("a");
     imageLink.href = result.links.html;
     imageLink.target = "_blank";
-    imageLink.textContent = result.alt_description;
+    imageLink.textContent = result.alt_description || "View Image";
 
     imageWrapper.appendChild(image);
     imageWrapper.appendChild(imageLink);
@@ -38,9 +43,7 @@ async function searchImages() {
 
   page++;
 
-  if (page > 1) {
-    showMoreButtonEl.style.display = "block";
-  }
+  showMoreButtonEl.style.display = "block";
 }
 
 formEl.addEventListener("submit", (event) => {
